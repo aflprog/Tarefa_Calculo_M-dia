cfeec37765caa509017c6661ad313e88c55d7275
Alexandre Ferreira <alexandre1984do@gmail.com>
2023-10-31 01:10:18 -0300

    Tarefa do módulo 8

---
+++ src/Tarefa_Calculo_Média.java
@@ -0,0 +1,18 @@
+public class Tarefa_Calculo_Média {
+
+        public static void main(String args[]){
+
+            calculaMedia();
+
+        }
+
+       public static void calculaMedia(){
+           int nota1=10;
+           int nota2=4;
+           int nota3=7;
+           int nota4=5;
+           int media=0;
+           media=(nota1+nota2+nota3+nota4)/4;
+           System.out.println("A média das notas é : "+media);
+    }
+}
